60962b2d0ea396978a2cd77bf7e7c8e9be8579bf
Tuanmoe <tuankhtnk20@gmail.com>
2023-09-07 16:20:08 +0700

    Update 07/09/2023

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+
+const Logo = () => {
+  const logoStyle={
+    'font-size':'2rem',
+    color:'blue',
+  }
+  return <span className='logo' style={logoStyle} > Jobster </span>;
+};
+export default Logo;
